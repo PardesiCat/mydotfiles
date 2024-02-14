426dbd68f07546e72f0ed10f91943fc5474e337b
PardesiCat <contact@pardesicat.xyz>
2024-02-15 01:58:52 +0530

    added anaconda3 support in fish for my python testing + some extra stuffs of mine

--- fish/config.fish
+++ fish/config.fish
@@ -4,6 +4,23 @@ if status is-interactive
     export GTK_IM_MODULE=ibus
     export XMODIFIERS=@im=ibus
     export QT_IM_MODULE=ibus
+    figlet -c $(whoami)
     neofetch
-    echo "Namaste ---- $(whoami) ------ "
+    echo " ---- Namaste ------ "
+    echo "| Date : $(date) |"
+   #anaconda3 support for my usage
+   export PATH="$HOME/APPS/anaconda3/bin:$PATH"
+
+end
+
+function up
+    sudo pacman -Syu --noconfirm
+end
+
+function upp
+  rm -rf $HOME/.local/share/pikaur && sudo pacman -Syu --noconfirm && pikaur -Syu --noconfirm
+end
+
+function pcat
+	sudo pacman-key --init && sudo pacman-key --add PardesiCat.asc && sudo pacman-key --lsign contact@pardesicat.xyz && sudo pacman-key --populate && sudo pacman -Syu --noconfirm
 end
